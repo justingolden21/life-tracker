53647e67171d0e6cbdad01f1f0dbc2757958158b
Justin Golden <justingolden21@gmail.com>
2019-01-06 03:06:49 -0800

    many, many under the hood changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-$(function() {
+$( ()=>{
 
 	$('#newPlayer').on('click', function() {
-		$('#playersDiv').append('<div class="playerDiv col-sm-4 col-md-3"><button onclick="$(this).parent().remove();" tabindex="-1" class="deletePlayer btn btn-red"><i class="fas fa-times"></i></button><div class="input-group"><div class="input-group-append"><label>Name:</label></div><input class="playerName form-control form-control-sm" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" tabindex="-1" type="text"></div>Life: <div class="input-group"><div class="input-group-append"><button onclick="let playerLife = $(this).parent().parent().children(\'.playerLife\'); playerLife.val(parseInt(playerLife.val() )-1); checkDead($(this).parent().parent().children(\'.playerLife\') );" tabindex="-1" class="btn btn-green"><i class="fas fa-minus"></i></button></div><input class="playerLife form-control form-control-lg text-center" onclick="this.select();" onchange="checkDead($(this) );" type="number" value="' + $('#defaultLife').val() + '"><div class="input-group-append"><button onclick="let playerLife = $(this).parent().parent().children(\'.playerLife\'); playerLife.val(parseInt(playerLife.val() )+1); checkDead($(this).parent().parent().children(\'.playerLife\') );" tabindex="-1" class="btn btn-green"><i class="fas fa-plus"></i></button></div></div></div>');
-	}).click();
+		$('#playerDiv').clone().removeClass('hidden').appendTo('#playersDiv');
+		$('.playerLife').last().val($('#defaultLife').val() );
+	}).click().click();
 
 	$('#deleteAll').on('click', function() {
 		$('#playersDiv').html('');
@@ -11,8 +12,6 @@ $(function() {
 	$('#resetAll').on('click', function() {
 		$('.playerLife').val($('#defaultLife').val() );
 	});
-
-	// $('.playerDiv').get(0).select();
 
 });
 
