ddf7883798ff66e238e8d7d3eeabcec1928a3e50
RGB Studios <justingolden21@gmail.com>
2019-01-25 01:22:47 -0800

    add url params

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
+let names = [];
+
 $( ()=>{
+
+	// load url params
+	let url = new URL(window.location.href);
+	let q = url.searchParams.get('q') || '';
+	names = q.split(',');
+	for(let i=0; i<names.length; i++) {
+		if(names[i]!='')
+			addCategory(names[i]);
+	}
+	$('#categoryName').val('Mana');
+
 
 	$('#newPlayer').click(function() {
 		$('#playerDiv').clone().removeClass('hidden').appendTo('#playersDiv');
@@ -14,17 +27,34 @@ $( ()=>{
 	});
 
 	$('#addCategory').click(function() {
-		$('.innerDiv').append('<br>' + $('#categoryName').val() + ':' );
-		$('#categoryDiv').clone().removeClass('hidden').appendTo('.innerDiv');
-
+		addCategory($('#categoryName').val() );
+		names.push($('#categoryName').val() );
 	});
 
 	$('#categoryName').click(function() {
 		$(this).select();
 	});
 
+	$('#getLink').click(function() {
+		history.replaceState({}, '', '?q=' + names);
+		copyLink();
+	});
+
 });
 
 function checkDead(elm) {
 	elm.val()<=0 ? elm.addClass('dead') : elm.removeClass('dead');
+}
+
+function addCategory(categoryName) {
+	$('.innerDiv').append('<br>' + categoryName + ':' );
+	$('#categoryDiv').clone().removeClass('hidden').appendTo('.innerDiv');
+}
+
+function copyLink() {
+	let tmp = $('<input type="text">').appendTo(document.body);
+	tmp.val(window.location.href);
+	tmp.select();
+	document.execCommand('copy');
+	tmp.remove();
 }
